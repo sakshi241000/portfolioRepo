c408f14d8640a1a06aeb0076da37b50ddc735950
shiwrale <sakshi_hiwrale@intuit.com>
2023-08-21 13:46:55 +0530

    fix: pportfolio

---
+++ src/portfolio.js
@@ -0,0 +1,379 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Sakshi Hiwrale",
+  title: "Hi all, I'm Sakshi",
+  subTitle: emoji(
+    "A passionate Front End Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / HTML / CSS and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1NqP7u-LcBFK0nS_AIxkkBCOrpIDBATYM/view?usp=drivesdk", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/sakshi241000",
+  linkedin: "https://www.linkedin.com/in/sakshi-hiwrale-893354170/",
+  gmail: "sakshihiwrale2410@gmail.com",
+  gitlab: "https://gitlab.com/sakshi241000",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FRONT END DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop  interactive Front end / User Interfaces for your web applications"
+    ),
+    emoji("⚡ Software Engineer with Strong analytical and Communication skills working at Persistent Systems."),
+    emoji(
+      "⚡  good eye for small details, and good at problem-solving, and have strong analytical skills"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Shri Sant Gajanan Maharaj College of Engineering",
+      subHeader: "Bachelor of Engineering",
+      duration: "August 2018 - June 2022",
+      desc: "Completed B.E. in Computer Science with 9.3 CGPA",
+      // descBullets: [
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      // ]
+    },
+    {
+      schoolName: "Z.P.Agarkar",
+      subHeader: "HSC",
+      duration: "2017 - 2018",
+      desc: "Percentage : 81%",
+      // descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    },
+    {
+      schoolName: "Saint Paul's Academy",
+      subHeader: "SSC",
+      duration: "2015 - 2016",
+      desc: "Percentage : 91%",
+      // descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "70%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Persistent Systems",
+      companylogo: require("./assets/images/psl.png"),
+      date: "June 2022 – Present",
+      desc: "Worked on UI Modernisation Project, QuickBooks, Intuit ",
+      descBullets: [
+        "Spearheaded a team of 30 skilled developers in a React JS based project for a well-known Application, delivering a total of 300+ contributions across various repositories.",
+        "Collaborated seamlessly with cross-functional teams to leverage React.js concepts, virtual DOM, and state management, resulting in significant refactoring and enhanced performance.",
+        "Transformed provided Figma designs into an immersive and seamless user interface, employing strong attention to detail and an eye for aesthetics.",
+        "Exhibited a meticulous approach to testing and debugging, ensuring the highest quality standards were met throughout the development process."
+      ]
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Persistent Systems Interns",
+      companylogo: require("./assets/images/psl.png"),
+      date: "Jan 2022 – Jun 2022",
+      desc: "Completed a 6-month internship focusing on React.js, excelling in developing interactive and dynamic web applications.Received comprehensive training in DBMS and Git, fostering effective collaboration and version control, while actively engaging in multiple mini projects to showcase versatility and dedication."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME PERSONAL AS WELL AS PROFESSIONAL PROJECTS I'VE WORKED ON ",
+  projects: [
+    {
+      image: require("./assets/images/qbo.png"),
+      projectName: "QUICKBOOKS",
+      projectDesc: ["Pioneered the creation of an innovative application using React.js, Redux, and Graph QL, streamlining the generation of Sales Receipts, Bills, Purchase Orders, and Expenses for Vendors, Customers, and Payees. Championed a quality-first approach by implementing extensive testing with Jest and React Testing Library, cementing thereliability and excellence of the application as a React Developer.\n",
+      "Tech Stack: - React, Redux, PostCSS, TypeScript, JavaScript, Jest, React Testing Library, Graph QL, ESLint,HTML5, CSS3 ,Storybook, Git."],
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://quickbooks.intuit.com/in/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+     image: require("./assets/images/ibm.png"),
+      projectName: "COVID-19 Twitter Sentiment Analysis",
+      projectDesc: "Developed Sentiment analysis model for COVID-related tweets, leveraging hashtags to fetch data .Designed to predict factors like the extension of lock downs and current COVID situation.Tech Stack: Node-RED,IBM Cloud ,JavaScript, Twitter API and Map box API",
+      footerLink: [
+        {
+          name: "Visit Website",
+          //url: "http://nextu.se/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      imageAlt: "Google Code-In Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+        },
+        {
+          name: "Award Letter",
+          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        },
+        {
+          name: "Google Code-in Blog",
+          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      imageAlt: "Google Assistant Action Logo",
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "false", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  //number: "+92-0000000000",
+  email_address: "sakshihiwrale2410@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "sakshihiwrale", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
